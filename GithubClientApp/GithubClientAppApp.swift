84fc9d52902074ab1338125dc86490f6ae6bd135
AR-morshed <arman.morshed98@gmail.com>
2025-06-09 11:59:43 +0600

    feat: project setup

---
+++ GithubClientApp/GithubClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientAppApp.swift
+//  GithubClientApp
+//
+//  Created by Arman Morshed on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
